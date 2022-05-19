02f0bd7fdfe577fd73ead859bf59ed5d01f32089
Olesya <asa88.88@bk.ru>
2022-05-19 17:27:03 +0300

    lesson3

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,5 @@
-const getData = () => {
-   return fetch('https://js-ozon-default-rtdb.firebaseio.com/goods.json')
+const getData = (str) => {
+   return fetch(`https://js-ozon-default-rtdb.firebaseio.com/goods.json`)
       .then((response) => {
          return response.json()
       })
